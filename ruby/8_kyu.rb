0647d8782e4a1d95668475ec477bf29b4169c0d0
seanchavez <realseanchavez@gmail.com>
2021-04-28 23:38:58 -0700

    sum of psitive, remove string spaces, reversed strings

--- ruby/8_kyu.rb
+++ ruby/8_kyu.rb
@@ -7,3 +7,19 @@ end
 def longest(a1, a2)
   a1.concat(a2).chars.uniq.sort.join
 end
+
+# sum of positive
+def positive_sum(arr)
+  positives = arr.select {|n| n > 0}
+  positives.sum
+end
+
+# remove string spaces
+def no_space(x)
+  x.split(' ').join
+end
+
+# reversed strings
+def solution(str)
+  str.reverse
+end
