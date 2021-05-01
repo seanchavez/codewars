a0d9d298dda874d12b5f22ab527d86bb9a1c0670
seanchavez <realseanchavez@gmail.com>
2021-04-30 18:35:08 -0700

    equal sides of array, remove minimum

--- ruby/7_kyu.rb
+++ ruby/7_kyu.rb
@@ -8,3 +8,18 @@ end
 def add_binary(a,b)
   (a + b).to_s(2)
 end
+
+# remove the minimum
+def remove_smallest(numbers)
+  new_list = []
+  numbers.each_with_index {|n, i| new_list << n unless i == numbers.index(numbers.min)}
+  new_list
+end
+
+# equal sides of an array
+def find_even_index(arr)
+  arr.each_index do |i| 
+    return i if arr[0,i].sum == arr[i + 1, arr.length - i].sum
+  end
+  -1
+end
