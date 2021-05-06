78b013b62ce14783232dc802ce1937ce969f6769
seanchavez <realseanchavez@gmail.com>
2021-05-06 13:02:20 -0700

    tickets(fixed)

--- ruby/7_kyu.rb
+++ ruby/7_kyu.rb
@@ -77,7 +77,6 @@ p dig_pow(695, 2) # should return 2 since 6² + 9³ + 5⁴= 1390 = 695 * 2
 p dig_pow(46288, 3) # should return 51 since 4³ + 6⁴+ 2⁵ + 8⁶ + 8⁷ = 2360688 = 46288 * 51
 
 def tickets(bills)
-  return "NO" if bills.empty?
   cash_drawer = {100 => 0, 50 => 0, 25 => 0} 
   bills.each do |bill|
     if bill > 25
@@ -88,7 +87,7 @@ def tickets(bills)
         if cash_drawer[50] == 0
           cash_drawer[25] < 3 ? (return "NO") : (cash_drawer[25] -= 3)
         else
-          cash_drawer[25] < 1 ? (return "NO") : (cash_drawer[25] -= 1)
+          cash_drawer[25] < 1 ? (return "NO") : (cash_drawer[25] -= 1 and cash_drawer[50] -= 1)
         end
       end
     end
@@ -100,5 +99,5 @@ end
 p tickets([25, 25, 50]) # => YES 
 p tickets([25, 100]) # => NO. Vasya will not have enough money to give change to 100 dollars
 p tickets([25, 25, 50, 50, 100]) # => NO. Vasya will not have the right bills to give 75 dollars of change (you can't make two bills of 25 from one of 50)
-p tickets([])
+p tickets([25, 25, 25, 25, 25, 25, 25, 50, 50, 50, 100, 100, 100, 100])
 
