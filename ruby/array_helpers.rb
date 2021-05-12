cc83056ce41cf1639af28cbd3229760f1432cfd5
seanchavez <realseanchavez@gmail.com>
2021-05-11 22:58:02 -0700

    array_helpers(broken)

--- ruby/array_helpers.rb
+++ ruby/array_helpers.rb
@@ -9,12 +9,20 @@ class Array
   end
 
   def average
-    self.sum / 2
+    self.reduce(:+) / 2
   end
 
   def sum
-    self.sum
+    self.reduce(:+)
   end
+end
+
+def even
+  self.select {|n| n.even?}
+end
+
+def odd
+  self.select {|n| n.odd?}
 end
 
 numbers = [1, 2, 3, 4, 5]
@@ -23,5 +31,5 @@ p numbers.square()  # must return [1, 4, 9, 16, 25]
 p numbers.cube()    # must return [1, 8, 27, 64, 125]
 p numbers.average() # must return 3
 p numbers.sum()     # must return 15
-# numbers.even()    # must return [2, 4]
-# numbers.odd()     # must return [1, 3, 5]
+p numbers.even()    # must return [2, 4]
+p numbers.odd()     # must return [1, 3, 5]
