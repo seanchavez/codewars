c27f872f0af08bfacc63c0c915566c12b2364bf9
seanchavez <realseanchavez@gmail.com>
2021-05-14 10:47:02 -0700

    optimize list_squared

--- ruby/7_kyu.rb
+++ ruby/7_kyu.rb
@@ -134,11 +134,25 @@ p reverse_alternate("") # ""
 def list_squared(m, n)
   list = []
   (m..n).each do |i|
-    sum_of_squares = (1..i).select {|x| i % x == 0}.map {|x| x ** 2}.sum
-    list << [i, sum_of_squares] if Math.sqrt(sum_of_squares) % 1 == 0  
+    sum_of_squares = get_divisors(i).map {|x| x ** 2}.sum
+    list << [i, sum_of_squares] if Math.sqrt(sum_of_squares) % 1 == 0
   end
   list
 end
 
+def get_divisors(num)
+  divisors = []
+  i = 1
+  loop do
+    if num % i == 0
+      num / i == i ? divisors.push(i) : divisors.push(i, num / i)
+    end
+    i += 1
+    break if i > Math.sqrt(num)
+  end
+  divisors
+end
+
 p list_squared(1, 250) # --> [[1, 1], [42, 2500], [246, 84100]]
 p list_squared(42, 250) # --> [[42, 2500], [246, 84100]]
+p get_divisors(100)
