5f429f6ea6eb8b121252cac1246a45365643c739
seanchavez <realseanchavez@gmail.com>
2021-05-15 16:50:34 -0700

    fields

--- ruby/7_kyu.rb
+++ ruby/7_kyu.rb
@@ -166,3 +166,19 @@ p url?('http://launchschool.com')   # -> true
 p url?('https://example.com')       # -> true
 p url?('https://example.com hello') # -> false
 p url?('   https://example.com')    # -> false
+
+def fields(str)
+  str.split(/[ \t,]+/)
+end
+
+p fields("Pete,201,Student")
+# -> ['Pete', '201', 'Student']
+
+p fields("Pete \t 201    ,  TA")
+# -> ['Pete', '201', 'TA']
+
+p fields("Pete \t 201")
+# -> ['Pete', '201']
+
+p fields("Pete \n 201")
+# -> ['Pete', '\n', '201']
