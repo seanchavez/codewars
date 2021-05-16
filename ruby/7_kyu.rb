687b527bf36a5dfe03a81c238af6bfb083a32165
seanchavez <realseanchavez@gmail.com>
2021-05-16 11:49:24 -0700

    format_date II

--- ruby/7_kyu.rb
+++ ruby/7_kyu.rb
@@ -173,13 +173,10 @@ end
 
 p fields("Pete,201,Student")
 # -> ['Pete', '201', 'Student']
-
 p fields("Pete \t 201    ,  TA")
 # -> ['Pete', '201', 'TA']
-
 p fields("Pete \t 201")
 # -> ['Pete', '201']
-
 p fields("Pete \n 201")
 # -> ['Pete', '\n', '201']
 
@@ -189,7 +186,6 @@ end
 
 p mystery_math('4 + 3 - 5 = 2')
 # -> '4 ? 3 - 5 = 2'
-
 p mystery_math('(4 * 3 + 2) / 7 - 1 = 1')
 # -> '(4 ? 3 + 2) / 7 - 1 = 1'
 
@@ -206,22 +202,26 @@ end
 
 p danish('An apple a day keeps the doctor away')
 # -> 'An danish a day keeps the doctor away'
-
 p danish('My favorite is blueberry pie')
 # -> 'My favorite is danish pie'
-
 p danish('The cherry of my eye')
 # -> 'The danish of my eye'
-
 p danish('apple. cherry. blueberry.')
 # -> 'danish. cherry. blueberry.'
-
 p danish('I love pineapple')
 # -> 'I love pineapple'
 
 def format_date(str)
-  str.gsub(/(\d\d\d\d)-(\d\d)-(\d\d)/, '\3.\2.\1')
+  str.sub(/(\d\d\d\d)-(\d\d)-(\d\d)/, '\3.\2.\1')
 end
 
 p format_date('2016-06-17') # -> '17.06.2016'
 p format_date('2016/06/17') # -> '2016/06/17' (no change)
+
+def format_date(str)
+  str.sub(/(\d\d\d\d)(-|\/)(\d\d)\2(\d\d)/, '\4.\3.\1')
+end
+
+p format_date('2016-06-17') # -> '17.06.2016'
+p format_date('2017/05/03') # -> '03.05.2017'
+p format_date('2015/01-31') # -> '2015/01-31' (no change)
