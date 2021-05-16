05aea2a39e450ec14b74c4bd8878f717ffce4d5b
seanchavez <realseanchavez@gmail.com>
2021-05-16 08:45:34 -0700

    mysterious_math

--- ruby/7_kyu.rb
+++ ruby/7_kyu.rb
@@ -192,3 +192,11 @@ p mystery_math('4 + 3 - 5 = 2')
 
 p mystery_math('(4 * 3 + 2) / 7 - 1 = 1')
 # -> '(4 ? 3 + 2) / 7 - 1 = 1'
+
+def mysterious_math(str)
+  str.gsub(/[+\-*\/]/, '?')
+end
+
+p mysterious_math('4 + 3 - 5 = 2')           # -> '4 ? 3 ? 5 = 2'
+p mysterious_math('(4 * 3 + 2) / 7 - 1 = 1') # -> '(4 ? 3 ? 2) ? 7 ? 1 = 1'
+
