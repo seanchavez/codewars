63303fff244f523bd5e5c3b9f5c18e3f1f4559d3
seanchavez <realseanchavez@gmail.com>
2021-05-16 08:56:56 -0700

    danish

--- ruby/7_kyu.rb
+++ ruby/7_kyu.rb
@@ -200,3 +200,22 @@ end
 p mysterious_math('4 + 3 - 5 = 2')           # -> '4 ? 3 ? 5 = 2'
 p mysterious_math('(4 * 3 + 2) / 7 - 1 = 1') # -> '(4 ? 3 ? 2) ? 7 ? 1 = 1'
 
+def danish(str)
+  str.sub(/\b(apple|blueberry|cherry)\b/, 'danish')
+end
+
+p danish('An apple a day keeps the doctor away')
+# -> 'An danish a day keeps the doctor away'
+
+p danish('My favorite is blueberry pie')
+# -> 'My favorite is danish pie'
+
+p danish('The cherry of my eye')
+# -> 'The danish of my eye'
+
+p danish('apple. cherry. blueberry.')
+# -> 'danish. cherry. blueberry.'
+
+p danish('I love pineapple')
+# -> 'I love pineapple'
+
