418c53814a199023b320b0e65a59b7c84426b6a5
seanchavez <realseanchavez@gmail.com>
2021-05-23 16:51:55 -0700

    multi_tap.rb

--- ruby/7_kyu.rb
+++ ruby/7_kyu.rb
@@ -225,24 +225,3 @@ end
 p format_date('2016-06-17') # -> '17.06.2016'
 p format_date('2017/05/03') # -> '03.05.2017'
 p format_date('2015/01-31') # -> '2015/01-31' (no change)
-
-def total_keypresses(str)
-  str.chars.reduce(0) do |presses, c|
-    char = c.downcase
-    presses + 1 + case char
-      when '2', 'a'..'c' then %w(a b c 2).index(char)
-      when '3', 'd'..'f' then %w(d e f 3).index(char)
-      when '4', 'g'..'i' then %w(g h i 4).index(char)
-      when '5', 'j'..'l' then %w(j k l 5).index(char)
-      when '6', 'm'..'o' then %w(m n o 6).index(char)
-      when '7', 'p'..'s' then %w(p q r s 7).index(char)
-      when '8', 't'..'v' then %w(t u v 8).index(char)
-      when '9', 'w'..'z' then %w(w x y z 9).index(char)
-      when '0' then 1
-      else 0
-    end
-  end
-end
-
-p total_keypresses('shit h8')
-p total_keypresses("WHERE DO U WANT 2 MEET L8R")
